c749e38d7e1e40f19f2b56ebff12ba793df18812
Andrei <danandreist@gmail.com>
2018-08-04 18:40:07 +0300

    added topics to watch

--- tools/mysql_database.go
+++ tools/mysql_database.go
@@ -6,7 +6,11 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-
+//topics:
+//msptokenmint: "0x0f6798a560793a54c3bcfe86a93cde1e73087d944c0ea20544137d4121396885"
+//cdr: "0x584eba3e36b122adb57f1e2f4ed1eb8e2b1bbbe1dc6241374239e45e4520a47d"
+//error: "0x57cf7a55e859b30b6bfeb9a7dd14411606106cb3e082f2cda387ec3b4b90be1c"
+//transferToken: "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
 
 var MDB *sqlx.DB
 
