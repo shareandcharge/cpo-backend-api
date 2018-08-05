7c786ba46e38ea3eeadc0b18f31f3b331c06ab65
Andrei <danandreist@gmail.com>
2018-08-05 21:11:44 +0300

    added topics to watch

--- configs/address_map.go
+++ configs/address_map.go
@@ -1,7 +1,7 @@
 package configs
 
 var AddressMap = make(map[string]string)
-
+var TopicsMap = make(map[string]string)
 
 //------- ADD TO THIS FUNCTION TO GET ADDRESS TO NAME  -------------
 func Init() {
@@ -10,6 +10,10 @@ func Init() {
 	AddressMap["0x7b0f2b531c018d4269a95561cfb4e038a7e3c8dc"] = "CPO 1"
 	AddressMap["0xf60b71a4d360a42ec9d4e7977d8d9928fd7c8365"] = "MSP"
 
+	TopicsMap["msptokenmint"] = "0x0f6798a560793a54c3bcfe86a93cde1e73087d944c0ea20544137d4121396885"
+	TopicsMap["cdr"] = "0x584eba3e36b122adb57f1e2f4ed1eb8e2b1bbbe1dc6241374239e45e4520a47d"
+	TopicsMap["error"] = "0x57cf7a55e859b30b6bfeb9a7dd14411606106cb3e082f2cda387ec3b4b90be1c"
+	TopicsMap["transferToken"] = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
 }
 
 func AddressToName(addr string) string {
